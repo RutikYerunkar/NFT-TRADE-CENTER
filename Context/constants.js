cd1a4d1df9ff34a9b03d2f7e1f29385176aa70ce
Rutik Yerunkar <rutik.yerunkar@cloudcounselage.com>
2023-02-04 19:17:08 +0530

    final commit

--- Context/constants.js
+++ Context/constants.js
@@ -5,10 +5,10 @@ import transferFunds from "./TransferFunds.json";
 
 //NFT MARKETPLACE
 export const NFTMarketplaceAddress =
-  "0xBd346A2bEeD00AaBbEd52A2Fe67166D3D4384567";
+  "0x38B7b5c259eE24509d82D298Fa593B8691f89Af9";
 export const NFTMarketplaceABI = nftMarketplace.abi;
 
 //TRANSFER FUNDS
 export const transferFundsAddress =
-  "0x70fA3b596f4f2fFc8ee731919D455acc5B4Ad62c";
+  "0x886f9E436f78738603c8cB46cF805f522725ed2e";
 export const transferFundsABI = transferFunds.abi;
