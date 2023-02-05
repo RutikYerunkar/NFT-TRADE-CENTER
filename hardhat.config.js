71bbd90387fbb3529c12aebacd175f4e040b265b
Rutik Yerunkar <rutik.yerunkar@cloudcounselage.com>
2023-02-05 10:05:36 +0530

    hardhat changes

--- hardhat.config.js
+++ hardhat.config.js
@@ -12,10 +12,10 @@ module.exports = {
       accounts: [`0x${"fcffe7577b8701fae6702646c38c8204d5daae9bc6e87c04fa2bd71ca52f4762"}`],
     },
 
-    fuji: {
-      url: "https://avalanche-fuji.infura.io/v3/84b588b22fe9f74b7b9a7fdef98e6151",
-      accounts: [`0x${"5de4111afa1a4b94908f83103eb1f1706367c2e68ca870fc3fb9a804cdab365a"}`],
-    }
+    // fuji: {
+    //   url: "https://avalanche-fuji.infura.io/v3/84b588b22fe9f74b7b9a7fdef98e6151",
+    //   accounts: [`0x${"5de4111afa1a4b94908f83103eb1f1706367c2e68ca870fc3fb9a804cdab365a"}`],
+    // }
 
   },
 };
